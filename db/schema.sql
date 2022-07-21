4434205bf7587e66b27071af14c0fd20b97ecfc3
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-07-21 17:02:25 -0500

    added 3 tables to my schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS tracker;
+
+CREATE DATABASE tracker;
+
+USE tracker;
+
+CREATE TABLE deparment (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10.2) NULL,
+    deparment_id INT NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NULL,
+    manager_id INT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE tracker;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+-- INSERT INTO department (name)
+-- VALUES ("Service");
+
+
