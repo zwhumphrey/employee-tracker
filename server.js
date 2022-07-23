d60278ecc2efc7764003f6a893d5f2bbdb4e2724
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-07-23 15:40:21 -0500

    added list of employees with department/salary/manager/and title

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ function viewDepartments() {
 function viewRoles() {
   console.log("Viewing all Roles!");
 
-  var query = `SELECT * FROM role`;
+  var query = `SELECT * FROM role LEFT JOIN department ON role = department`;
 
   db.query(query, function (err, res) {
     if (err) throw err;
@@ -125,9 +125,14 @@ function viewRoles() {
 // add join to view job/department/salary/manager
 //View Employees
 function viewEmployees() {
-  console.log("Viewing all Employees!");
-
-  var query = `SELECT * FROM employee`;
+  var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`;
 
   db.query(query, function (err, res) {
     if (err) throw err;
